7d552a8c0483b2faa3572473adcdf7f2520a5900
u21514951 <u21514951@tuks.co.za>
2023-09-29 15:15:12 +0200

    IFrame + UM

--- Frontend/src/app/HelpIFrames/ProcDetailIFrame/proc-detail-iframe/proc-detail-iframe.component.ts
+++ Frontend/src/app/HelpIFrames/ProcDetailIFrame/proc-detail-iframe/proc-detail-iframe.component.ts
@@ -7,7 +7,7 @@ import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
   styleUrls: ['./proc-detail-iframe.component.css']
 })
 export class ProcDetailIFrameComponent {
-  presetImageUrl = 'assets/Images/IFrameNewViewProcDetails.png';
+  presetImageUrl = 'assets/Images/IFrameProcurementDetails.png';
 
   constructor(private sanitizer: DomSanitizer) {}
 
